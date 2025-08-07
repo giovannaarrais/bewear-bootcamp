f3eea73f2528c66df17c3ff6148804be06a11956
giovannaarrais <giovannaarrais13@gmail.com>
2025-08-06 22:54:30 -0300

    :chore: add better-auth tables

--- src/db/index.ts
+++ src/db/index.ts
@@ -1,5 +1,11 @@
 import "dotenv/config";
 
 import { drizzle } from "drizzle-orm/node-postgres";
-export const db = drizzle(process.env.DATABASE_URL!);
- 
+
+//importa o arq do schema dos forms
+import * as schema from "./schema";
+
+// chama ele aqui para o db saber 
+export const db = drizzle(process.env.DATABASE_URL!, {
+    schema,
+});
--- src/db/schema.ts
+++ src/db/schema.ts
@@ -1,10 +1,58 @@
-import {  relations } from "drizzle-orm";
-import { integer, pgTable, text, timestamp,uuid } from "drizzle-orm/pg-core";
+import { relations } from "drizzle-orm";
+import {
+  boolean,
+  integer,
+  pgTable,
+  text,
+  timestamp,
+  uuid,
+} from "drizzle-orm/pg-core";
 
-//tabela de usuários
 export const userTable = pgTable("user", {
-    id: uuid().primaryKey().defaultRandom(),
-    name: text().notNull(),
+    id: text("id").primaryKey(),
+    name: text("name").notNull(),
+    email: text("email").notNull().unique(),
+    emailVerified: boolean("email_verified")
+    .$defaultFn(() => false)
+    .notNull(),
+    image: text("image"),
+    createdAt: timestamp("created_at")
+        .$defaultFn(() => /* @__PURE__ */ new Date())
+        .notNull(),
+    updatedAt: timestamp("updated_at")
+        .$defaultFn(() => /* @__PURE__ */ new Date())
+        .notNull(),
+});
+
+export const session = pgTable("session", {
+    id: text("id").primaryKey(),
+    expiresAt: timestamp("expires_at").notNull(),
+    token: text("token").notNull().unique(),
+    createdAt: timestamp("created_at").notNull(),
+    updatedAt: timestamp("updated_at").notNull(),
+    ipAddress: text("ip_address"),
+    userAgent: text("user_agent"),
+    userId: text("user_id")
+        .notNull()
+        .references(() => userTable.id, { onDelete: "cascade" }),
+});
+
+export const account = pgTable("account", {
+    id: text("id").primaryKey(),
+    accountId: text("account_id").notNull(),
+    providerId: text("provider_id").notNull(),
+    userId: text("user_id")
+        .notNull()
+        .references(() => userTable.id, { onDelete: "cascade" }),
+    accessToken: text("access_token"),
+    refreshToken: text("refresh_token"),
+    idToken: text("id_token"),
+    accessTokenExpiresAt: timestamp("access_token_expires_at"),
+    refreshTokenExpiresAt: timestamp("refresh_token_expires_at"),
+    scope: text("scope"),
+    password: text("password"),
+    createdAt: timestamp("created_at").notNull(),
+    updatedAt: timestamp("updated_at").notNull(),
 });
 
 // tabela de categoria e de produtos possuem relacionamento
@@ -15,14 +63,16 @@ export const categoryTable = pgTable("category", {
     name: text().notNull(),
     slug: text().notNull().unique(), // faz parte da url, contribui no SEO
     createdAt: timestamp().notNull().defaultNow(), //data de criação
-})
+});
 
 // tabela de produtos
 export const productTable = pgTable("product", {
     id: uuid().primaryKey().defaultRandom(),
     categoryId: uuid("category_id")
-    .notNull()
-    .references(() => categoryTable.id), // esse registro é a relacao q existe com a table de categorias
+        .notNull()
+        .references(() => categoryTable.id, {onDelete: 'set null'}), // esse registro é a relacao q existe com a table de categorias
+        //set null -> for deletado da tabela productTable, o campo productId nas tabelas relacionadas será setado como null.
+        
     name: text().notNull(),
     slug: text().notNull().unique(), // faz parte da url, contribui no SEO
     description: text().notNull(),
@@ -33,41 +83,44 @@ export const productTable = pgTable("product", {
 });
 
 // tabela para lidar com variacoes de produtos: uma roupa de diferentes cores
-export const productVariantTable = pgTable("product_variant" , {
+export const productVariantTable = pgTable("product_variant", {
     id: uuid().primaryKey().defaultRandom(),
     productId: uuid("product_id")
-    .notNull()
-    .references(() => productTable.id),
+        .notNull()
+        .references(() => productTable.id, {onDelete: "cascade"}),
+        //cascade -> todos os registros que dependem dele também serão deletados
     name: text().notNull(),
-    slug: text().notNull().unique(), 
+    slug: text().notNull().unique(),
     color: text().notNull(), // permite a busca pela cor
     priceInCents: integer("prince_in_cents").notNull(), // nessa logica salva o preço em centavos
     imageUrl: text("image_url").notNull(),
     createdAt: timestamp("created_at").notNull().defaultNow(), //data de criação
-})
+});
 
 // ---------------- Relacionamentos
 
 // uma categoria pode ter varios produtos
 export const categoryRelations = relations(categoryTable, ({ many }) => ({
     products: many(productTable),
-}))
+}));
 
-
-// um produto pode ter apenas uma categoria 
+// um produto pode ter apenas uma categoria
 // o categoryId do produto referencia ao id da tabela de categoria
-export const productRelations = relations(productTable , ({ one, many }) => ({
+export const productRelations = relations(productTable, ({ one, many }) => ({
     category: one(categoryTable, {
         fields: [productTable.categoryId],
-        references: [categoryTable.id]
+        references: [categoryTable.id],
     }),
-    variants: many(productVariantTable) // indica q um produto pode ter varias variantes: cor, modelo, tamanho
-}))
+  variants: many(productVariantTable), // indica q um produto pode ter varias variantes: cor, modelo, tamanho
+}));
 
 // o productId da variante se referencia ao id da tabela de produtos
-export const productVariantRelations = relations(productVariantTable, ({one}) => ({
+export const productVariantRelations = relations(
+    productVariantTable,
+    ({ one }) => ({
         product: one(productTable, {
-            fields: [productVariantTable.productId],
-            references: [productTable.id]
-        })
-}))
+        fields: [productVariantTable.productId],
+        references: [productTable.id],
+        }),
+    }),
+);
