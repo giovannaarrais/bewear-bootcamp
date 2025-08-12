87eb6d553f506cce93521bd1a2ed9bb9a0c4f27e
giovannaarrais <giovannaarrais13@gmail.com>
2025-08-11 22:10:23 -0300

    :sparkles: feat: add parcial page products pt.2

--- src/app/product-variant/[slug]/page.tsx
+++ src/app/product-variant/[slug]/page.tsx
@@ -1,11 +1,15 @@
 import { eq } from "drizzle-orm";
+import Image from "next/image";
 import { notFound } from "next/navigation";
 import React from "react";
 
+import Footer from "@/components/common/footer";
 import Header from "@/components/common/header";
+import ProductsList from "@/components/common/products-list";
+import { Button } from "@/components/ui/button";
 import { db } from "@/db";
-import {  productVariantTable } from "@/db/schema";
-import Image from "next/image";
+import {  productTable, productVariantTable } from "@/db/schema";
+import { FormatCentsToBRL } from "@/helpers/money";
 
 interface ProductVariantPageProps {
     params: Promise<{ slug: string }>;
@@ -26,6 +30,15 @@ const ProductVariantPage = async ({ params }: ProductVariantPageProps) => {
     if (!productVariant) {
         return notFound();
     }
+
+    // captura os produtos dessa mesma categoria que pertence ao produto visualizado
+    const likelyProducts = await db.query.productTable.findMany({
+        where: eq(productTable.categoryId, productVariant.product.categoryId),
+        with: {
+            variants: true
+        }
+    })
+
     return (
         <div>
             <Header />
@@ -37,10 +50,56 @@ const ProductVariantPage = async ({ params }: ProductVariantPageProps) => {
                     sizes="100vw"
                     width={0}
                     height={0}
-                    className="h-auto w-full rounded-3xl"
+                    className="h-auto w-full"
                 />
-
             </div>
+
+            {/* VARIANTES */}
+            <div className="px-5">
+                
+            </div>
+
+            <div className="px-5 space-y-7 mb-6">
+                {/* DESCRICAO */}   
+                <div className="desc">
+                    <h3 className="text-lg font-semibold">
+                        {productVariant.product.name}
+                    </h3>
+                    <h2 className="text-muted-foreground text-sm">
+                        {productVariant.name}
+                    </h2>
+                    <h3 className="text-lg font-semibold">
+                        {FormatCentsToBRL(productVariant.priceInCents)}
+                    </h3>
+                </div>
+
+                {/* QUANTIDADE DESEJADA */}
+                <div>
+
+                </div>
+                
+                {/* ACOES */}
+                <div className="space-y-3 flex flex-col">
+                    <Button variant="outline" className="rounded-3xl py-6 font-semibold"> 
+                        Adicionar à sacola 
+                    </Button>
+                    <Button variant="default" className="rounded-3xl py-6 font-semibold"> 
+                        Comprar Agora 
+                    </Button>
+                </div>
+
+                <div className="">
+                    <p className="text-sm">
+                        {productVariant.product.description}
+                    </p>
+                </div>
+            </div>
+
+            {/* Lista de produtos com a mesma categoria */}
+            <ProductsList products={likelyProducts} title="Você tambêm pode gostar"/>
+
+            <Footer />
+
         </div>
     );
 };
