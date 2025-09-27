30ba94d532ee15d456f0af630d4407e76576a585
giovannaarrais <giovannaarrais13@gmail.com>
2025-09-26 22:09:52 -0300

    :sparkles: feat: ensure order can be created

--- src/app/cart/confirmation/components/finish-order-button.tsx
+++ src/app/cart/confirmation/components/finish-order-button.tsx
@@ -1,27 +1,76 @@
 "use client"
 
 import { Loader2 } from 'lucide-react';
-import React from 'react';
+import Image from 'next/image';
+import React, { useState } from 'react';
 
 import { Button } from '@/components/ui/button';
+import {
+    Dialog,
+    DialogContent,
+    DialogDescription,
+    DialogFooter,
+    DialogHeader,
+    DialogTitle,
+    DialogTrigger,
+  } from "@/components/ui/dialog"
 import { useFinishOrder } from '@/hooks/mutations/use-finish-order';
 
 const FinishOrderButton = () => {
 
+    const [successDialogIsOpen, setSuccessDialogIsOpen ] = useState(true)
     const finishOrderMutation = useFinishOrder();
 
     return (
-        <Button 
-            className='mt-3 w-full rounded-full' 
-            size="lg"
-            onClick={() => finishOrderMutation.mutate()}
-            disabled={finishOrderMutation.isPending}
-        >
-            {finishOrderMutation.isPending && (
-                <Loader2  className='h-4 w-4 animate-spin'/>
-            )}
-            Finalizar Compra
-        </Button>
+        <>
+            <Button 
+                className='mt-3 w-full rounded-full' 
+                size="lg"
+                onClick={() => finishOrderMutation.mutate()}
+                disabled={finishOrderMutation.isPending}
+            >
+                {finishOrderMutation.isPending && (
+                    <Loader2  className='h-4 w-4 animate-spin'/>
+                )}
+                Finalizar Compra
+            </Button>
+
+            <Dialog 
+                open={successDialogIsOpen} 
+                onOpenChange={setSuccessDialogIsOpen}
+            >
+                <DialogContent>
+                    <DialogHeader>
+                        <Image 
+                            src="/msgs/illustration.png" 
+                            alt='Imagem ilustrativa de sucesso' 
+                            width={251}
+                            height={233}
+                            className='pb-7 m-auto'
+                        />
+                        <DialogTitle>Pedido Efetuado</DialogTitle>
+                    </DialogHeader>
+                    <DialogDescription className='font-medium text-center'>
+                        Seu pedido foi efetuado com sucesso. Você pode acompanhar o status na seção de “Meus Pedidos”.
+                    </DialogDescription>
+                    <DialogFooter>
+                        <Button 
+                        variant='default'
+                        size='lg'
+                        className='mt-3 w-full rounded-full'>
+                            Ver meus pedidos
+                        </Button>
+
+                        <Button 
+                        variant='outline'
+                        className='mt-3 w-full rounded-full'
+                        size='lg'>
+                            Página Inicial
+                        </Button>
+                    </DialogFooter>
+                </DialogContent>
+            </Dialog>
+        </>
     );
 };
 
