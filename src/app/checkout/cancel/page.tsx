7e92c7aba202774704430eda54714e3f0a055d20
giovannaarrais <giovannaarrais13@gmail.com>
2025-10-02 22:03:52 -0300

    :sparkles: feat: add stripe webhook api route

---
+++ src/app/checkout/cancel/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const CheckoutCancel = () => {
+    return (
+        <div>
+        <h1>Checkout cancel</h1>
+        </div>
+    );
+};
+
+export default CheckoutCancel;
