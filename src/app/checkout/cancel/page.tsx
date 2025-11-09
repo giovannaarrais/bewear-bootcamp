19a99f9f75804313c7d83ee3ae610700848e618e
giovannaarrais <giovannaarrais13@gmail.com>
2025-11-09 14:01:07 -0300

    :bug: fix: text para corrigir problema no build

--- src/app/checkout/cancel/page.tsx
+++ src/app/checkout/cancel/page.tsx
@@ -2,7 +2,7 @@ import Image from "next/image";
 import Link from "next/link";
 import React from "react";
 
-import { Button } from '@/components/ui/button';
+import { Button } from "@/components/ui/button";
 import {
     Dialog,
     DialogContent,
@@ -11,53 +11,51 @@ import {
     DialogHeader,
     DialogTitle,
     DialogTrigger,
-  } from "@/components/ui/dialog"
+} from "@/components/ui/dialog";
 
 const CheckoutCancelPage = () => {
-  return (
-    <>
-    <Dialog 
-          open={true} 
-      >
-          <DialogContent>
-              <DialogHeader>
-                  <Image
-                      src="/msgs/cancel_pay.svg" 
-                      alt='Imagem ilustrativa de sucesso' 
-                      width={251}
-                      height={233}
-                      className='pb-7 m-auto'
-                  />
-                  <DialogTitle>Pagamento Cancelado</DialogTitle>
-              </DialogHeader>
-              <DialogDescription className='font-medium text-center'>
-                  Seu pedido não foi efetuado. Para tentar novamente, clique no botão abaixo.
-              </DialogDescription>
-              <DialogFooter className='flex sm:flex-col'>
-                  <Button 
-                  variant='default'
-                  className='mt-3 w-full rounded-full'
-                  size='lg'
-                  asChild>
-                      <Link href="/cart/confirmation">
-                          Tentar novamente 
-                      </Link>
-                  </Button>
+    return (
+        <>
+            <Dialog open={true}>
+                <DialogContent>
+                <DialogHeader>
+                    <Image
+                    src="/msgs/cancel_pay.svg"
+                    alt="Imagem ilustrativa de sucesso"
+                    width={251}
+                    height={233}
+                    className="m-auto pb-7"
+                    />
+                    <DialogTitle>Pagamento Cancelado</DialogTitle>
+                </DialogHeader>
+                <DialogDescription className="text-center font-medium">
+                    Seu pedido não foi efetuado. Para tentar novamente, clique no botão
+                    abaixo.
+                </DialogDescription>
+                <DialogFooter className="flex sm:flex-col">
+                    <Button
+                    variant="default"
+                    className="mt-3 w-full rounded-full"
+                    size="lg"
+                    asChild
+                    >
+                    <Link href="/cart/confirmation">Tentar novamente</Link>
+                    </Button>
 
-                  <Button
-                  variant='outline'
-                  size='lg'
-                  className='mt-3 w-full rounded-full'
-                  asChild>
-                      <Link href='/checkout/my-orders'>
-                          Ver Meus Pedidos
-                      </Link>
-                  </Button>
-              </DialogFooter>
-          </DialogContent>
-      </Dialog>
-    </>
-  );
+                    <Button
+                    variant="outline"
+                    size="lg"
+                    className="mt-3 w-full rounded-full"
+                    asChild
+                    >
+                    <Link href="/checkout/my-orders">Ver Meus Pedidos</Link>
+                    </Button>
+                </DialogFooter>
+                
+                </DialogContent>
+            </Dialog>
+        </>
+    );
 };
 
 export default CheckoutCancelPage;
